391d863b0f19de4e9077a5fb111b9d0f30edeb74
Arbi Kullakshi <19522436-somethim@users.noreply.gitlab.com>
2024-01-24 14:14:46 +0100

    binary_search

--- main.cpp
+++ main.cpp
@@ -5,102 +5,34 @@
 
 using namespace std;
 
-//# Used:
-//# 1. vector
-//# 2. struct
-//# 4. remove_if
-//# 5. erase
-//# 6. auto
-//# 7. range based for loop
-//# 8. pointer
 
+int binary_search(const int arr[], int x, int low, int high) {
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
 
-//#TODO:
-//     1. Add Contact (input only name and phone number, id should be auto generated)
-//     2. Search Contact (search by id, name or phone number. Print all details of contact)
-//     3. Delete Contact (search by id, name or phone number. Delete contact using id)
-//     4. List Contacts (print all contacts)
-//     5. Exit()
-
-struct Contact {
-    int id;
-    string name;
-    string phoneNumber;
-};
-
-vector<Contact> contacts;
-
-void addContact(const Contact &contact) {
-    contacts.push_back(contact);
-}
-
-Contact *searchContact(const string &name) {
-    for (auto &contact: contacts) {
-        if (contact.name == name) {
-            return &contact;
+        if (arr[mid] == x) {
+            return mid;
+        } else if (arr[mid] < x) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
         }
     }
-    return nullptr;
-}
 
-void deleteContact(const string &name) {
-    contacts.erase(remove_if(contacts.begin(), contacts.end(),
-                             [&](const Contact &contact) { return contact.name == name; }),
-                   contacts.end());
-}
-
-void listContacts() {
-    for (const auto &contact: contacts) {
-        cout << "Name: " << contact.name << ", Phone Number: " << contact.phoneNumber << "\n";
-    }
+    return -1;
 }
 
 int main() {
-    int choice;
+    int arr[] = {3, 4, 5, 6, 7, 8, 9};
+    int x = 4;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int result = binary_search(arr, x, 0, n - 1);
+    if (result == -1) {
+        cout << "Element not found";
+    } else {
+        cout << "Element found at index " << result;
+    }
 
-    do {
-        cout << "1. Add Contact\n2. Search Contact\n3. Delete Contact\n4. List Contacts\n5. Exit\n";
-        cin >> choice;
-
-        switch (choice) {
-            case 1: {
-                Contact newContact;
-                cout << "Enter name: ";
-                cin >> newContact.name;
-                cout << "Enter phone number: ";
-                cin >> newContact.phoneNumber;
-                addContact(newContact);
-                break;
-            }
-            case 2: {
-                string searchName;
-                cout << "Enter name: ";
-                cin >> searchName;
-                Contact *foundContact = searchContact(searchName);
-                if (foundContact) {
-                    cout << "Name: " << foundContact->name << ", Phone Number: " << foundContact->phoneNumber << "\n";
-                } else {
-                    cout << "Contact not found\n";
-                }
-                break;
-            }
-            case 3: {
-                string deleteName;
-                cout << "Enter name: ";
-                cin >> deleteName;
-                deleteContact(deleteName);
-                break;
-            }
-            case 4:
-                listContacts();
-                break;
-            case 5:
-                break;
-            default:
-                cout << "Invalid choice\n";
-                break;
-        }
-    } while (choice != 5);
 
     return 0;
 }
